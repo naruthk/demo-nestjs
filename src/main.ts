5f1299555014d07007aa6af20d3b86c9723c4401
Naruth Kongurai <nkongurai@gmail.com>
2021-05-07 17:06:44 +0700

    use env credentials and support multi environments

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server'); // corresponds to the key in ./config/default.yml file
+
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
-
   logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
